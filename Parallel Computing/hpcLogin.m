12a418e000610e73d7a7509a64ab70bd74b56459
Owen S <93752382+OwenS1919@users.noreply.github.com>
2025-01-17 16:03:06 +1000

    Delete Parallel Computing directory

--- Parallel Computing/hpcLogin.m
+++
@@ -1,6 +0,0 @@
-function hpcLogin()
-% just using this to automate the hpc login
-
-fprintf("ssh stewar51@lyra.qut.edu.au\n")
-
-end
--- Parallel Computing/testFuncParallel.m
+++
@@ -1,5 +0,0 @@
-function output = testFuncParallel()
-
-output = eig(rand(10));
-
-end
